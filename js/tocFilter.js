49b7813d0651ce7ebac9cffbbd51038bdfec11d2
sergioSHKLR <sergio@shklr.org>
2023-08-05 13:05:33 -0500

    navbar menu template

--- js/tocFilter.js
+++ js/tocFilter.js
@@ -1,12 +1,12 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+function toggleShow() {
+  document.getElementById("myDropdown2").classList.toggle("show");
 }
 
 function filterFunction() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
+  div = document.getElementById("myDropdown2");
   a = div.getElementsByTagName("a");
   for (i = 0; i < a.length; i++) {
     txtValue = a[i].textContent || a[i].innerText;
