bc74d140584935cb036361c7a9317200ab2b6823
sergioSHKLR <sergio@shklr.org>
2023-08-07 21:35:53 -0500

    large to x-large

--- js/increaseFont.js
+++ js/increaseFont.js
@@ -1,4 +1,4 @@
 function increaseFontsize() {
   let mainDiv = document.getElementById("main");
-  mainDiv.style.fontSize = 'large'; // change font size to xx-large
+  mainDiv.style.fontSize = 'x-large'; // change font size to xx-large
 }
