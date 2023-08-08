54625f9153e985430b4d94de03ada3565e5349b1
sergioSHKLR <sergio@shklr.org>
2023-08-07 20:29:13 -0500

    rev css media queries

---
+++ js/toggleShow1.js
@@ -0,0 +1,16 @@
+function toggleShow1() {
+  document.getElementById("myDropdown1").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn1')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content1");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
