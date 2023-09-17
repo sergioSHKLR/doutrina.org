1c8881076ec888edb902c9a599c7560bcdf928e9
sergioSHKLR <sergio@shklr.org>
2023-09-16 23:18:24 -0500

    sync

--- js/combined.js
+++ js/combined.js
@@ -114,10 +114,10 @@ function toggleContentTop() {
     x.style.display = "block";
   }
   var x = document.getElementById("open-external");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
+  if (x.style.display === "none") {
     x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 }
 
