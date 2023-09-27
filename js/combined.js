0643b5329cbe1fe1158c7fa878eb02a9c594d80d
sergioSHKLR <sergio@shklr.org>
2023-09-27 15:48:12 -0500

    fix combined.js

--- js/combined.js
+++ js/combined.js
@@ -169,16 +169,3 @@ function decreaseFontsize() {
   let mainDiv = document.getElementById("content-panel");
   mainDiv.style.fontSize = 'small'; // change font size to xx-large
 }
-
-const EL_body = document.querySelector("content-panel");
-const ELS_fontSize = document.querySelectorAll(".fontSize");
-localStorage.fontSize = localStorage.fontSize || 16; // Read or default to 16px
-function changeSize() {
-  EL_body.style.fontSize = `${localStorage.fontSize}px`;
-}
-ELS_fontSize.forEach(el => el.addEventListener("click", function () {
-  localStorage.fontSize = parseInt(localStorage.fontSize) + parseInt(el.value);
-  changeSize();
-}));
-// Change size on subsequent page load
-changeSize();
