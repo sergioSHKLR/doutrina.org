c09e4526bbbf4d050f75945d879e1bbf9b612735
sergioSHKLR <sergio@shklr.org>
2023-10-09 01:11:54 -0500

    desktop best

---
+++ js/lean.js
@@ -0,0 +1,61 @@
+// this js handles fullscreen, side panel, notes panels and anti-distraction curtain.
+
+
+// PANELS
+
+// open right panel
+function openPanel() {
+  document.getElementById("sidebar").style.display = "block";
+  document.getElementById("menu").style.display = "none";
+}
+
+// close right panel
+function closePanel() {
+  document.getElementById("sidebar").style.display = "none";
+  document.getElementById("menu").style.display = "block";
+}
+
+// open notes panel
+function openNotes() {
+  document.getElementById("notes").style.display = "block";
+}
+
+// close notes panel
+function closeNotes() {
+  document.getElementById("notes").style.display = "none";
+}
+
+// FULLSCREEN
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
+// CURTAIN
+
+// toggles anti-distraction curtain
+function toggleShades() {
+  var element = document.getElementById("ext-iframe");
+  element.classList.toggle("shades-on");
+}
