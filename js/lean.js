c549bf453e25e2e82526fa712f3b4ca8c94714a6
sergioSHKLR <sergio@shklr.org>
2023-10-09 19:53:38 -0500

    sync

--- js/lean.js
+++ js/lean.js
@@ -59,3 +59,22 @@ function toggleShades() {
   var element = document.getElementById("ext-iframe");
   element.classList.toggle("shades-on");
 }
+
+
+//TABBED CONTENT
+
+function openTab(evt, tabName) {
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+      document.getElementById(tabName).style.display = "block";
+      evt.currentTarget.className += " active";
+    }
+
+    document.getElementById("defaultOpen").click();
