bd272e3b256e87443ea184af2d0636fad9d3be23
sergioSHKLR <sergio@shklr.org>
2023-10-16 06:44:38 -0500

    hero

---
+++ framework/js/crisp.js
@@ -0,0 +1,1 @@
+window.$crisp = []; window.CRISP_WEBSITE_ID = "3008a719-1db4-494b-bb64-ec74931a394d"; (function () { d = document; s = d.createElement("script"); s.src = "https://client.crisp.chat/l.js"; s.async = 1; d.getElementsByTagName("head")[0].appendChild(s); })();
