158f6a17aff366337b09ad03375991a3a5a93c4c
sergioSHKLR <sergio@shklr.org>
2023-10-23 17:27:31 -0500

    white

---
+++ framework/js/scripts1.js
@@ -0,0 +1,133 @@
+
+
+// DARK MODE --------------------------------------------------
+
+function activeDark2() {
+    var element = document.body;
+    element.classList.add("dark-mode");
+    var element = document.getElementById("col2-dark-mode");
+    element.classList.toggle("hide");
+    var element = document.getElementById("col2-light-mode");
+    element.classList.toggle("show");
+}
+
+function inactiveDark2() {
+    var element = document.body;
+    element.classList.remove("dark-mode");
+    var element = document.getElementById("col2-dark-mode");
+    element.classList.toggle("hide");
+    var element = document.getElementById("col2-light-mode");
+    element.classList.toggle("show");
+}
+
+function activeDark3() {
+    var element = document.body;
+    element.classList.add("dark-mode");
+    var element = document.getElementById("col3-dark-mode");
+    element.classList.toggle("hide");
+    var element = document.getElementById("col3-light-mode");
+    element.classList.toggle("show");
+}
+
+function inactiveDark3() {
+    var element = document.body;
+    element.classList.remove("dark-mode");
+    var element = document.getElementById("col3-dark-mode");
+    element.classList.toggle("hide");
+    var element = document.getElementById("col3-light-mode");
+    element.classList.toggle("show");
+}
+
+//---------------------------------------------------------------
+
+// FULLSCREEN ---------------------------------------------------
+
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen2() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+    var element = document.getElementById("col2-full-screen");
+    element.classList.toggle("hide");
+    var element = document.getElementById("col2-normal-screen");
+    element.classList.toggle("show");
+}
+
+/* Close fullscreen */
+function closeFullscreen2() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+    var element = document.getElementById("col2-full-screen");
+    element.classList.toggle("hide");
+    var element = document.getElementById("col2-normal-screen");
+    element.classList.toggle("show");
+}
+
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen3() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+    var element = document.getElementById("col3-full-screen");
+    element.classList.toggle("hide");
+    var element = document.getElementById("col3-normal-screen");
+    element.classList.toggle("show");
+}
+
+/* Close fullscreen */
+function closeFullscreen3() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+    var element = document.getElementById("col3-full-screen");
+    element.classList.toggle("hide");
+    var element = document.getElementById("col3-normal-screen");
+    element.classList.toggle("show");
+}
+
+//--------------------------------------------------------------
+
+// SHADE ------------------------------------------------------
+
+function shadesOn() {
+    var element = document.getElementById("ext-iframe");
+    element.classList.add("shades-on");
+    element.classList.remove("shades-off");
+    var element = document.getElementById("col3-shades-on");
+    element.classList.toggle("hide");
+    var element = document.getElementById("col3-shades-off");
+    element.classList.toggle("show");
+}
+
+function shadesOff() {
+    var element = document.getElementById("ext-iframe");
+    element.classList.add("shades-off");
+    element.classList.remove("shades-on");
+    var element = document.getElementById("col3-shades-on");
+    element.classList.toggle("hide");
+    var element = document.getElementById("col3-shades-off");
+    element.classList.toggle("show");
+}
+
+//--------------------------------------------------------------
