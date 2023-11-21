c3e0facdcc4c4d79a9ca7fd43e493d5d0882c211
sergioSHKLR <sergio@shklr.org>
2023-11-21 00:09:56 -0600

    sync

---
+++ framework/js/menu.js
@@ -0,0 +1,16 @@
+
+// MENU ----------------------------------------------------------
+
+// MENU ON -------------------------------------------------------
+function menuOn() {
+    document.getElementById("col1-body").style.display = 'inline-block';
+    document.getElementById("col2-menu-on").style.display = 'none';
+    document.getElementById("col2-menu-off").style.display = 'inline-block';
+}
+
+// MENU OFF -------------------------------------------------------
+function menuOff() {
+    document.getElementById("col1-body").style.display = 'none';
+    document.getElementById("col2-menu-off").style.display = 'none';
+    document.getElementById("col2-menu-on").style.display = 'inline-block';
+}
