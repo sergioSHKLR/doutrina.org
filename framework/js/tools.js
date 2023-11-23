3f2d9b2ff5cca67a087032251ad78fae5c920964
sergioSHKLR <sergio@shklr.org>
2023-11-23 07:16:57 -0600

    tools final

--- framework/js/tools.js
+++ framework/js/tools.js
@@ -1,10 +1,28 @@
+function menuDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
 
 //---------------------------------------------------
 function function1() {
-    localStorage.setItem("text","increased");
+    localStorage.setItem("text", "increased");
     var element = document.getElementById("col2-body");
     element.classList.remove("td");
-    element.classList.toggle("ti");
+    element.classList.remove("tn");
+    element.classList.add("ti");
 }
 
 function checkMode1() {
@@ -15,17 +33,33 @@ function checkMode1() {
 }
 
 //---------------------------------------------------
-
-function function2() {
-    localStorage.setItem("text","decreased");
+function function0() {
+    localStorage.setItem("text", "normal");
     var element = document.getElementById("col2-body");
     element.classList.remove("ti");
+    element.classList.remove("td");
+    element.classList.add("tn");
+}
+
+function checkMode0() {
+    let text = localStorage.getItem("text");
+    if (text == "normal") {
+        function0()
+    }
+}
+//---------------------------------------------------
+
+function function2() {
+    localStorage.setItem("text", "decreased");
+    var element = document.getElementById("col2-body");
+    element.classList.remove("ti");
+    element.classList.remove("tn");
     element.classList.add("td");
 }
 
 function checkMode2() {
     let text = localStorage.getItem("text");
-    if (text == "increased") {
+    if (text == "decreased") {
         function2()
     }
 }
@@ -33,7 +67,7 @@ function checkMode2() {
 //---------------------------------------------------
 
 function function3() {
-    localStorage.setItem("align","left");
+    localStorage.setItem("align", "left");
     var element = document.getElementById("col2-body");
     element.classList.remove("aj");
     element.classList.add("al");
@@ -49,7 +83,7 @@ function checkMode3() {
 //---------------------------------------------------
 
 function function4() {
-    localStorage.setItem("align","justified");
+    localStorage.setItem("align", "justified");
     var element = document.getElementById("col2-body");
     element.classList.remove("al");
     element.classList.add("aj");
@@ -65,7 +99,7 @@ function checkMode4() {
 //---------------------------------------------------
 
 function function5() {
-    localStorage.setItem("font","sans");
+    localStorage.setItem("font", "sans");
     var element = document.getElementById("col2-body");
     element.classList.remove("f2");
     element.classList.remove("f3");
@@ -83,7 +117,7 @@ function checkMode5() {
 //---------------------------------------------------
 
 function function6() {
-    localStorage.setItem("font","serif");
+    localStorage.setItem("font", "serif");
     var element = document.getElementById("col2-body");
     element.classList.remove("f1");
     element.classList.remove("f3");
@@ -101,7 +135,7 @@ function checkMode6() {
 //---------------------------------------------------
 
 function function7() {
-    localStorage.setItem("font","mono");
+    localStorage.setItem("font", "mono");
     var element = document.getElementById("col2-body");
     element.classList.remove("f1");
     element.classList.remove("f2");
@@ -119,7 +153,7 @@ function checkMode7() {
 //---------------------------------------------------
 
 function function8() {
-    localStorage.setItem("font","slab");
+    localStorage.setItem("font", "slab");
     var element = document.getElementById("col2-body");
     element.classList.remove("f1");
     element.classList.remove("f2");
@@ -137,9 +171,10 @@ function checkMode8() {
 //---------------------------------------------------
 
 function function9() {
-    localStorage.setItem("density","small");
+    localStorage.setItem("density", "small");
     var element = document.getElementById("col2-body");
     element.classList.remove("dm");
+    element.classList.remove("dn");
     element.classList.add("ds");
 }
 
@@ -151,11 +186,28 @@ function checkMode9() {
 }
 
 //---------------------------------------------------
+function function9a() {
+    localStorage.setItem("density", "normal");
+    var element = document.getElementById("col2-body");
+    element.classList.remove("dm");
+    element.classList.remove("dn");
+    element.classList.add("dn");
+}
+
+function checkMode9a() {
+    let density = localStorage.getItem("density");
+    if (density == "normal") {
+        function9a()
+    }
+}
+
+//---------------------------------------------------
 
 function function10() {
-    localStorage.setItem("density","medium");
+    localStorage.setItem("density", "medium");
     var element = document.getElementById("col2-body");
     element.classList.remove("ds");
+    element.classList.remove("dn");
     element.classList.add("dm");
 }
 
@@ -169,7 +221,7 @@ function checkMode10() {
 //---------------------------------------------------
 
 function function11() {
-    localStorage.setItem("bground","amarelado");
+    localStorage.setItem("bground", "amarelado");
     var element = document.getElementById("col2-body");
     element.classList.remove("b2");
     element.classList.remove("b3");
@@ -186,10 +238,11 @@ function checkMode11() {
 //---------------------------------------------------
 
 function function12() {
-    localStorage.setItem("bground","esverdeado");
+    localStorage.setItem("bground", "esverdeado");
     var element = document.getElementById("col2-body");
     element.classList.remove("b1");
     element.classList.remove("b3");
+    element.classList.remove("b4");
     element.classList.add("b2");
 }
 
@@ -203,10 +256,11 @@ function checkMode12() {
 //---------------------------------------------------
 
 function function13() {
-    localStorage.setItem("bground","roseado");
+    localStorage.setItem("bground", "roseado");
     var element = document.getElementById("col2-body");
     element.classList.remove("b1");
     element.classList.remove("b2");
+    element.classList.remove("b4");
     element.classList.add("b3");
 }
 
@@ -218,4 +272,79 @@ function checkMode13() {
 }
 
 //---------------------------------------------------
+
+
+function function14() {
+    localStorage.setItem("bground", "branco");
+    var element = document.getElementById("col2-body");
+    element.classList.remove("b1");
+    element.classList.remove("b2");
+    element.classList.remove("b3");
+    element.classList.add("b4");
+}
+
+function checkMode14() {
+    let bground = localStorage.getItem("bground");
+    if (bground == "branco") {
+        function14()
+    }
+}
+
+//---------------------------------------------------
+
+function checkMode() {
+    let text = localStorage.getItem("text");
+    if (text == "increased") {
+        function1()
+    }
+    if (text == "normal") {
+        function0()
+    }
+    if (text == "decreased") {
+        function2()
+    }
+    let align = localStorage.getItem("align");
+    if (align == "left") {
+        function3()
+    }
+    if (align == "justified") {
+        function4()
+    }
+    let font = localStorage.getItem("font");
+    if (font == "sans") {
+        function5()
+    }
+    if (font == "serif") {
+        function6()
+    }
+    if (font == "mono") {
+        function7()
+    }
+    if (font == "slab") {
+        function8()
+    }
+    let density = localStorage.getItem("density");
+    if (density == "small") {
+        function9()
+    }
+    if (density == "normal") {
+        function9a()
+    }
+    if (density == "medium") {
+        function10()
+    }
+    let bground = localStorage.getItem("bground");
+    if (bground == "amarelado") {
+        function11()
+    }
+    if (bground == "esverdeado") {
+        function12()
+    }
+    if (bground == "roseado") {
+        function13()
+    }
+    if (bground == "branco") {
+        function14()
+    }
+}
 
