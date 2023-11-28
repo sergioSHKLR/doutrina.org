d2b7b5a6f81554089b1a08ac4c7121d1c1129e29
sergio SHKLR <107352082+sergioSHKLR@users.noreply.github.com>
2023-11-27 19:48:11 -0600

    sync

--- framework/js/ext-links.js
+++ framework/js/ext-links.js
@@ -1,7 +1,7 @@
 
 // SETS COL2 LINKS TO TARGET EXT --------------------------------
 (function () {
-  const links = document.querySelectorAll("a[href^='https://pt.m.wikipedia'], a[href^='https://pt.m.wiktionary'], a[href^='https://www.bible'], a[href^='https://www.youtube.com/embed']")
+  const links = document.querySelectorAll("a[href^='https://pt.m.wikipedia'], a[href^='https://pt.m.wiktionary'], a[href^='https://www.bible'], a[href^='https://www.youtube.com/embed'], a[href^='https://www.luzespirita.org.br']")
   const host = window.location.hostname
 
   const isInternalLink = link => new URL(link).hostname === host
