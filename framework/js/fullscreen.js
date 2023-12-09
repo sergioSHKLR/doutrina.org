199cd93a753697e9fe708e59dff2eb7dc0aa4f3a
sergioSHKLR <sergio@shklr.org>
2023-12-09 04:09:32 -0600

    fullscreen

--- framework/js/fullscreen.js
+++ framework/js/fullscreen.js
@@ -1,8 +1,8 @@
 // FULLSCREEN ---------------------------------------------------
 var elem = document.documentElement;
 
-// OPEN FULLSCREEN COL2 -----------------------------------------
-function openFullscreen2() {
+// OPEN FULLSCREEN  -----------------------------------------
+function openFullscreen() {
     if (elem.requestFullscreen) {
         elem.requestFullscreen();
     } else if (elem.webkitRequestFullscreen) { /* Safari */
@@ -16,8 +16,8 @@ function openFullscreen2() {
     element.classList.toggle("show");
 }
 
-// CLOSE FULLSCREEN COL2 -----------------------------------------
-function closeFullscreen2() {
+// CLOSE FULLSCREEN  -----------------------------------------
+function closeFullscreen() {
     if (document.exitFullscreen) {
         document.exitFullscreen();
     } else if (document.webkitExitFullscreen) { /* Safari */
@@ -32,34 +32,3 @@ function closeFullscreen2() {
 }
 
 var elem = document.documentElement;
-
-// OPEN FULLSCREEN COL3 ------------------------------------------
-function openFullscreen3() {
-    if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-    } else if (elem.webkitRequestFullscreen) { /* Safari */
-        elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { /* IE11 */
-        elem.msRequestFullscreen();
-    }
-    var element = document.getElementById("col3-fs-btn");
-    element.classList.toggle("hide");
-    var element = document.getElementById("col3-ns-btn");
-    element.classList.toggle("show");
-}
-
-// CLOSE FULLSCREEN COL3 -----------------------------------------
-function closeFullscreen3() {
-    if (document.exitFullscreen) {
-        document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) { /* Safari */
-        document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) { /* IE11 */
-        document.msExitFullscreen();
-    }
-    var element = document.getElementById("col3-ns-btn");
-    element.classList.toggle("show");
-    var element = document.getElementById("col3-fs-btn");
-    element.classList.toggle("hide");
-
-}
