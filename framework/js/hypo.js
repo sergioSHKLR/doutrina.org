7a6d77f6f902030fe28d0e6d4eebee3f7dd52336
sergioSHKLR <sergio@shklr.org>
2023-12-12 19:57:11 -0600

    sync

--- framework/js/hypo.js
+++ framework/js/hypo.js
@@ -1,3 +1,3 @@
 function hypoAlert() {
-    alert("Caso a aba de anotações não esteja abrindo, clique e arraste o botão (seta) para a esquerda");
+    alert("Caso a aba de anotações não esteja abrindo, clique e arraste o botão do topo.");
 }
