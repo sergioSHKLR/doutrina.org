205ae91885e496cc8f44ddaf0d4d31dfe5ef1691
sergioSHKLR <sergio@shklr.org>
2023-12-17 13:03:37 -0600

    pwa fix webshare off hypo off

--- framework/js/scripts.js
+++ framework/js/scripts.js
@@ -484,26 +484,5 @@ function checkMode() {
 
 // TOOLS END
 
-// WEBSHARE START
 
-const shareData = {
-    title: "💧 doutrina.org",
-    text: "❤️ Amai-vos e 🎓 instruí-vos!",
-    url: "https://doutrina.org",
-};
-
-const btn = document.querySelector("#webshare");
-const resultPara = document.querySelector(".result");
-
-// Share must be triggered by "user activation"
-btn.addEventListener("click", async () => {
-    try {
-        await navigator.share(shareData);
-        resultPara.textContent = "Compartilhado com sucesso!";
-    } catch (err) {
-        resultPara.textContent = `Erro ao compartilhar`;
-    }
-});
-
-// WEBSHARE END
 
