e165b1404b84672a0d1a416f76a721c85ac4bf47
sergioSHKLR <sergio@shklr.org>
2023-12-22 08:05:14 -0600

    pwa zip

---
+++ app.md
@@ -0,0 +1,6 @@
+---
+title: App
+layout: blank
+---
+
+[app](./framework/pwa/apk/doutrina.org.apk)
---
+++ framework/pwa/apk/source/app/src/main/java/org/doutrina/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package org.doutrina.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
