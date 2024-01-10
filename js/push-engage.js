881da4012f494a51fc26e206ee674a8e7fa48e18
sergioSHKLR <sergio@shklr.org>
2024-01-10 09:57:02 -0600

    push

--- js/push-engage.js
+++ js/push-engage.js
@@ -1,9 +1,9 @@
 
-(function(w, d) {
+(function (w, d) {
   w.PushEngage = w.PushEngage || [];
   w._peq = w._peq || [];
   PushEngage.push(['init', {
-      appId: '824a9da9-f658-4480-b925-198aa9b078c8'
+    appId: '824a9da9-f658-4480-b925-198aa9b078c8'
   }]);
 
   var e = d.createElement('script');
