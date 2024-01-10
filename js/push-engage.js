1b499308f3b8184b62abb1e95b18682d29f93d36
sergio SHKLR <107352082+sergioSHKLR@users.noreply.github.com>
2024-01-10 05:15:32 -0600

    Update js

---
+++ js/push-engage.js
@@ -0,0 +1,15 @@
+
+(function(w, d) {
+  w.PushEngage = w.PushEngage || [];
+  w._peq = w._peq || [];
+  PushEngage.push(['init', {
+      appId: '824a9da9-f658-4480-b925-198aa9b078c8'
+  }]);
+
+  var e = d.createElement('script');
+
+  e.src = 'https://clientcdn.pushengage.com/sdks/pushengage-web-sdk.js';
+  e.async = true;
+  e.type = 'text/javascript';
+  d.head.appendChild(e);
+})(window, document);
