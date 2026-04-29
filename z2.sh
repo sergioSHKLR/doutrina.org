5332c330adaa0aba1328e0634ac3fc6d8eb8cda3
sergioSHKLR <sergio.shklr@gmail.com>
2026-04-29 11:40:31 -0300

    Final: simple copy from doutrina-content + remove submodule traces

--- z2.sh
+++ z2.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-sudo bundle exec jekyll serve --port 7003 --incremental
+#!/bin/bash
+echo "Updating content from doutrina-content..."
+sudo rm -rf books 2>dev/null
+sudo cp -r ../doutrina-content/books .
+
+echo "Building Jekyll site..."
+sudo rm -rf _site
+sudo bundle exec jekyll build
+
+echo "Done! Check _sitee/books/"
